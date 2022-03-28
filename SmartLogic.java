7e104c8dea72976538e88790c89e25ddb3e0c376
LKrivaa <90308251+LKrivaa@users.noreply.github.com>
2022-03-28 16:35:21 +0200

    trying to implement rules

--- SmartLogic.java
+++ SmartLogic.java
@@ -1,3 +1,7 @@
+import net.sf.javabdd.BDD;
+import net.sf.javabdd.BDDFactory;
+import net.sf.javabdd.JFactory;
+
 public class SmartLogic implements IQueensLogic{
 
     /*TO RUN IN COMMAND PROMPT: java Queens.java SmartLogic 6 */ 
@@ -10,10 +14,16 @@ public class SmartLogic implements IQueensLogic{
 
     private int size;		// Size of quadratic game board (i.e. size = #rows = #columns)
     private int[][] board;	// Content of the board. Possible values: 0 (empty), 1 (queen), -1 (no queen allowed)
-    
+    private BDDFactory factory;
+    private BDD True;
+    private BDD False;
+    private BDD bdd;
+    private int n = 8;
+
     public void initializeBoard(int size) {
         this.size = size;
         this.board = new int[size][size];
+        buildBDD();
     }
    
     public int[][] getBoard() {
@@ -21,12 +31,85 @@ public class SmartLogic implements IQueensLogic{
     }
 
     public void insertQueen(int column, int row) {
-        
+        board[column][row] = 1;
     }   
     
+    public void buildBDD() {
+        //Factory
+        this.factory = JFactory.init(2000000, 200000); // set buffer etc.
+
+        //64 fields => 64 variables
+        this.factory.setVarNum(this.n*this.n);
+
+        //For clarity
+        this.False = this.factory.zero();
+        this.True = this.factory.one();
+
+        //Initalize our bdd to true
+        this.bdd = True;
+
+        //Add the rules to the bdd
+        createEightRule();
+    }
+
+    public void createEightRule(){
+
+        for (int y = 0; y < n; y++) {
+            BDD sub_bdd = False;
+           
+          for (int x = 0; x < n; x++) {
+            sub_bdd = sub_bdd.or(this.factory.ithVar(place(x,y)));
+          }
+          
+          //sub_bdd must be true
+          this.bdd = this.bdd.and(sub_bdd);
+        }
+    }
+
+    private int place(int column,int row) {
+        return row*this.n+column;
+      }
+
+
     /** Use n*n variables and make a method for retrieving the correct 
      * variable or variable number for a given position (column and row).*/
     public int retrieveVariable(){
         return 1;
+    }
+
+    private boolean placeInvalid(int column,int row) {
+        //add queen at x ,y
+        BDD test_bdd = this.bdd.restrict(this.factory.ithVar(place(column,row))); 
+        //check if unsatisfiable?
+        return test_bdd.isZero();
+      }
+
+      private void updateInvalid() {
+        //For each cell, check if placing a queen there makes it invalid
+        //If so, make that places graphic be -1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (placeInvalid(i,j)) {
+                  board[i][j] = -1;
+                }
+            }
+        }
+    }
+     
+    public boolean queenInsert(int column, int row) {
+
+        if (board[column][row] == -1 || board[column][row] == 1) {
+            return true;
+        }
+        
+        //Set a queen in graphic
+        board[column][row] = 1;
+
+        //Set a queen in the bdd
+        this.bdd = this.bdd.restrict(this.factory.ithVar(row*this.n+column)); 
+
+        updateInvalid();
+
+        return true;
     }
 }
